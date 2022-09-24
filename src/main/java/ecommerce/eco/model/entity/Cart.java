3aca5ebab37292299562501d4ad4c2a419055080
Abel3581 <marzoa3581@gmail.com>
2022-09-24 19:14:48 -0300

    agregado model cart review y sus relaciones

--- src/main/java/ecommerce/eco/model/entity/Cart.java
+++ src/main/java/ecommerce/eco/model/entity/Cart.java
@@ -1,4 +1,40 @@
 package ecommerce.eco.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import ecommerce.eco.util.CartEnum;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private CartEnum state;// estado
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    private boolean softDeleted = Boolean.FALSE;
+
+    @CreationTimestamp
+    @Column(name = "registration", updatable = false, nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime registration;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
