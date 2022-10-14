9e80bd96145a5c4e265bef4fa5b4508ea7e3dceb
gabynavarro <alt64b@gmail.com>
2022-10-14 17:56:37 -0300

    paypal thymeleaf testing

--- src/main/java/ecommerce/eco/controller/PaypalController.java
+++ src/main/java/ecommerce/eco/controller/PaypalController.java
@@ -5,6 +5,9 @@ import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 import ecommerce.eco.model.paypal.Order;
 import ecommerce.eco.service.PaypalService;
+import ecommerce.eco.service.ProductServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +18,9 @@ public class PaypalController {
     @Autowired
     PaypalService service;
 
-    public static final String SUCCESS_URL = "paypal/success";
-    public static final String CANCEL_URL = "paypal/cancel";
-
+    public static final String SUCCESS_URL = "/pay/success";
+    public static final String CANCEL_URL = "/pay/cancel";
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaypalController.class);
   @GetMapping("/home")
     public String home() {
         return "home";
@@ -25,11 +28,13 @@ public class PaypalController {
 
     @PostMapping("/pay")
     public String payment(@ModelAttribute("order") Order order) {
+        LOGGER.warn(order.getDescription());
         try {
             Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
                     order.getIntent(), order.getDescription(),
-                    "http://localhost:8090/" + CANCEL_URL,
-                    "http://localhost:8090/" + SUCCESS_URL);
+                    "http://localhost:8090/paypal" + CANCEL_URL,
+                    "http://localhost:8090/paypal" + SUCCESS_URL);
+
             for(Links link:payment.getLinks()) {
                 if(link.getRel().equals("approval_url")) {
                     return "redirect:"+link.getHref();
@@ -41,12 +46,12 @@ public class PaypalController {
         return "redirect:/";
     }
 
-    @GetMapping("value = CANCEL_UR")
+    @GetMapping(value ="CANCEL_UR")
     public String cancelPay() {
         return "cancel";
     }
 
-    @GetMapping("value = SUCCESS_URL")
+    @GetMapping(value ="SUCCESS_URL")
     public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
         try {
             Payment payment = service.executePayment(paymentId, payerId);
